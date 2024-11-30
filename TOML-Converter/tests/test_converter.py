8a9b6220592d91fff97987de8cb51b35a80a1066
anastasia09lekhno <lekhno.anastasia@gmail.com>
2024-11-30 12:58:50 +0300

    Update test_converter.py

--- TOML-Converter/tests/test_converter.py
+++ TOML-Converter/tests/test_converter.py
@@ -5,26 +5,19 @@ from config_converter.parser import parse_toml
 
 class TestConverter(unittest.TestCase):
     def test_simple_toml(self):
-        # Входные данные TOML
         toml_data = """
         [server]
         ip = "127.0.0.1"
         port = 8080
         port2 = "|port+1|"
         """
-        # Ожидаемый результат
-        expected_output = "'(server'(ip [[127.0.0.1]])'(port 8080)'(port2 8081))"
+        expected_output = "'( [[server]] '([[ip]] [[127.0.0.1]]) '([[port]] 8080) '([[port2]] 8081) )"
 
-        # Парсим TOML
         parsed_data = parse_toml(toml_data)
-        # Конвертируем в целевой формат
         output = convert_to_custom_language(parsed_data)
-
-        # Сравнение результата с ожидаемым
         self.assertEqual(output, expected_output)
 
     def test_nested_structure(self):
-        # Входные данные TOML
         toml_data = """
         [server]
         ip = "192.168.0.1"
@@ -34,34 +27,22 @@ class TestConverter(unittest.TestCase):
         cores = 4
         memory = 16
         """
-        # Ожидаемый результат
-        expected_output = "'(server'(ip [[192.168.0.1]])'(role [[backend]])'(details'(cores 4)'(memory 16)))"
+        expected_output = "'( [[server]] '([[ip]] [[192.168.0.1]]) '([[role]] [[backend]]) '([[details]] '([[cores]] 4) '([[memory]] 16) ) )"
 
-        # Парсим TOML
         parsed_data = parse_toml(toml_data)
-        # Конвертируем в целевой формат
         output = convert_to_custom_language(parsed_data)
-
-        # Сравнение результата с ожидаемым
         self.assertEqual(output, expected_output)
 
     def test_constants_calculation(self):
-        # Входные данные TOML
         toml_data = """
-        [calculation]
-        base = 100
-        increment = 50
-        total = "|base + increment|"
+        [ports]
+        auth = 8080
+        worker = "|auth + 1|"
         """
-        # Ожидаемый результат
-        expected_output = "'(calculation'(base 100)'(increment 50)'(total 150))"
+        expected_output = "'( [[ports]] '([[auth]] 8080) '([[worker]] 8081) )"
 
-        # Парсим TOML
         parsed_data = parse_toml(toml_data)
-        # Конвертируем в целевой формат
         output = convert_to_custom_language(parsed_data)
-
-        # Сравнение результата с ожидаемым
         self.assertEqual(output, expected_output)
 
 
